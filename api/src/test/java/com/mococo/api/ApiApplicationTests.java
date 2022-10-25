c2a2802bbd367dccbb7f6c4019415bb01528dc25
Kyungtae Park <74488924+ktae23@users.noreply.github.com>
2022-10-26 00:19:59 +0900

    initial commit

---
+++ api/src/test/java/com/mococo/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mococo.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
